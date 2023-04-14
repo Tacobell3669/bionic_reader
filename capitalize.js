cba674b56461acaf0075122602f4814a1c8af9c4
Tacobell3669 <73504045+Tacobell3669@users.noreply.github.com>
2023-04-14 16:12:53 -0400

    Update capitalize.js

--- capitalize.js
+++ capitalize.js
@@ -1,37 +1,52 @@
-// Get the file input element from the HTML DOM
-const fileInput = document.querySelector('input[type="file"]');
+// Load the PDF file using pdf.js
+const pdfData = await fetch('input.pdf').then((res) => res.arrayBuffer());
+const pdf = await pdfjsLib.getDocument({ data: pdfData }).promise;
 
-// Add an event listener to the file input element to handle file selection
-fileInput.addEventListener('change', handleFileSelect);
+// Iterate through each page in the PDF
+for (let i = 1; i <= pdf.numPages; i++) {
+  const page = await pdf.getPage(i);
 
-function handleFileSelect(event) {
-  const file = event.target.files[0];
-  
-  // Create a new FileReader object
-  const reader = new FileReader();
-  
-  // Add an event listener to the FileReader object to handle file reading
-  reader.addEventListener('load', handleFileRead);
-  
-  // Read the file as text
-  reader.readAsText(file);
-}
+  // Get the text content of the page
+  const textContent = await page.getTextContent();
 
-function handleFileRead(event) {
-  const text = event.target.result;
-  
-  // Capitalize the first letter of every word in the text
-  const capitalizedText = text.replace(/\b\w/g, (match) => match.toUpperCase());
-  
-  // Create a new Blob object from the capitalized text
-  const blob = new Blob([capitalizedText], { type: 'text/plain' });
-  
-  // Create a new anchor element to download the modified file
+  // Capitalize the first letter of each word in the text content
+  const modifiedTextContent = textContent.items
+    .map((item) => ({
+      ...item,
+      str: item.str.replace(/\b\w/g, (match) => match.toUpperCase()),
+    }))
+    .reduce((acc, item) => acc + item.str, '');
+
+  // Create a new canvas element to render the modified text
+  const viewport = page.getViewport({ scale: 1 });
+  const canvas = document.createElement('canvas');
+  canvas.width = viewport.width;
+  canvas.height = viewport.height;
+
+  // Render the modified text to the canvas
+  const canvasContext = canvas.getContext('2d');
+  await page.render({
+    canvasContext,
+    viewport,
+    textContent: pdfjsLib.getTextContent(modifiedTextContent),
+  }).promise;
+
+  // Convert the canvas to a new PDF page
+  const newPage = await pdfjsLib.createPage(canvas);
+  const newPageViewport = newPage.getViewport({ scale: 1 });
+
+  // Create a new PDF document and add the new page
+  const newPdf = await pdfjsLib.getDocument({}).promise;
+  const newPdfPage = await newPdf.createPage(newPageViewport);
+  await newPdfPage.drawPage(newPage);
+  await newPdf.addPage(newPdfPage);
+
+  // Save the new PDF file to disk
+  const newPdfData = await newPdf.saveDocument();
+  const blob = new Blob([newPdfData], { type: 'application/pdf' });
   const downloadLink = document.createElement('a');
   downloadLink.href = URL.createObjectURL(blob);
-  downloadLink.download = 'modified.txt';
-  
-  // Simulate a click on the download link to trigger file download
+  downloadLink.download = 'modified.pdf';
   document.body.appendChild(downloadLink);
   downloadLink.click();
   document.body.removeChild(downloadLink);
